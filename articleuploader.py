d73b9958ee6cd2c8e1e4026ea4544facf2e09381
Noel Reed <noelreed3@gmail.com>
2025-10-13 16:12:50 -0500

    Skip over already existing articles, unless --force is specified

--- articleuploader.py
+++ articleuploader.py
@@ -1,6 +1,20 @@
 import os
+from argparse import ArgumentParser
 import pywikibot
 from pywikibot.page import Page
+
+argument_parser = ArgumentParser(
+    description="Upload articles from the generatedarticles folder into a"
+                + "wikimedia family (specified in user-config.py)"
+)
+
+argument_parser.add_argument(
+    "--force", "-f",
+    action='store_true',
+    help="overwrite articles that already exist instead of skipping them",
+)
+
+cmd_args = argument_parser.parse_args()
 
 site = pywikibot.Site('en','local')
 
@@ -14,6 +28,11 @@ for article in articles:
     index += 1
     print(f"{index}/{len(articles)}: {article}")
     page = Page(site,article)
+
+    if page.exists() and not cmd_args.force:
+        print("Skipping because the article already exists")
+        print("Use --force to overwrite the article anyways")
+        continue
     
     with open(os.path.join('generatedarticles',article)) as f:
         page.text = f.read()
